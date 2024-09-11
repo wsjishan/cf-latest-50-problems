0d2a3beee292f916dfceb645048189f18994125c
wsjishan <wahidsadikjishan@gmail.com>
2024-09-11 20:39:13 +0600

    chore: Add lazy loading for images in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { inject } from "@vercel/analytics";
+inject();
 // Populate rating select options
 const ratings = [
   800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000,
